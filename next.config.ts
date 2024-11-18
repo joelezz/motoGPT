6d2b89225fc841c971f2c73513f758062b8985dd
Joel Ezzahid <77654189+joelezz@users.noreply.github.com>
2024-11-18 06:02:19 +0200

    Update next.config.ts
    making deployment ready

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",  // <=== enables static exports
   reactStrictMode: true,
-  
-  images: {
-      domains: ['localhost'],
-    },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
